b5209637b9103405be9764c17e50bcda6a69c372
freelife11 <mirrol33@gmail.com>
2024-10-22 13:52:19 +0900

    10.22 업데이트1
    sass 설치 및 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="example">
+    <h1>hello world blue test</h1>
+  </div>
 </template>
 
 <script>
