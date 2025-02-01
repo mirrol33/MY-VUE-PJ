fead8599ce630724eb11e4a6f5ee5bfb24418b22
LMK <mirrol33@gmail.com>
2025-02-02 01:41:03 +0900

    02.02 update1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <!-- src/App.vue -->
 <template>
   <div>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <header>
+      <div class="header">
+        <nav>
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <div class="inner">
+        <p>2025 FREELIFE11</p>
+      </div>
+    </footer>
   </div>
 </template>
 
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="main-wrap">
-    <h2>main page</h2>
-    <p>test</p>
+    <section class="section-0">
+      <div class="video-area">
+        <video class="video" ref="video" autoplay muted loop playsinline>
+          <source src="../images/video/1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </section>
+    <section class="section-1">
+      <div class="inner">
+        <div class="intro-01">
+          <p>Introducing the first-ever web design asset collection curated in the Pantone Color of the Year 2025. Predesigned yet fully customizable, this limited edition capsule gets you ahead of trends the rest of the world is only waking up to.</p>
+        </div>
+        <div class="intro-02">
+          <h2>어쩌구 저쩌구구</h2>
+        </div>
+      </div>
+    </section>
+    <section class="section-2">
+      <div class="inner"></div>
+    </section>
   </div>
 </template>
 
