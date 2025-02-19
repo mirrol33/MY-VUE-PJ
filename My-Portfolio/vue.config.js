e0c3ab8b7677b79fdbb218ac15463b7959123e35
LMK <mirrol33@gmail.com>
2025-02-19 23:11:41 +0900

    02.19 update18

--- My-Portfolio/vue.config.js
+++ My-Portfolio/vue.config.js
@@ -3,15 +3,14 @@
 //   transpileDependencies: true
 // })
 
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production' ? '/MY-VUE-PJ/my-portfolio/' : '/',
+//   // outputDir: "docs",
+// })
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/MY-VUE-PJ/my-portfolio/' : '/',
-  // outputDir: "docs",
-})
-
-// module.exports = {
-//   publicPath: '/MY-VUE-PJ/my-portfolio/',  // GitHub Pages 경로에 맞게 수정
-//   transpileDependencies: true
-// }
+module.exports = {
+  publicPath: '/MY-VUE-PJ/my-portfolio/',  // GitHub Pages 경로에 맞게 수정
+  transpileDependencies: true
+}
